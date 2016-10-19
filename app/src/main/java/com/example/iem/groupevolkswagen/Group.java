3407b4ab65b2adb423231089fec235b391a36b48
Robin Turon-Lagot <robin.turon-lagot@etu.univlyon1.fr>
2016-10-19 15:07:11 +0200

    add and rm add to Group and SubComp

--- app/src/main/java/com/example/iem/groupevolkswagen/Group.java
+++ app/src/main/java/com/example/iem/groupevolkswagen/Group.java
@@ -19,5 +19,5 @@ public class Group {
         this.volkswagen.remove(subComp);
     }
 
-    
+
 }
--- app/src/main/java/com/example/iem/groupevolkswagen/SubComp.java
+++ app/src/main/java/com/example/iem/groupevolkswagen/SubComp.java
@@ -1,5 +1,8 @@
 package com.example.iem.groupevolkswagen;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by iem on 19/10/2016.
  */
@@ -8,6 +11,7 @@ public class SubComp {
 
     protected String name,origin;
     protected int revenue2015, nbEmployee, rebuyDate;
+    protected List<Vehicule> lVehicle = new ArrayList<>();
 
     public SubComp() {
 
@@ -19,5 +23,13 @@ public class SubComp {
         this.revenue2015 = revenue2015;
         this.nbEmployee = nbEmployee;
         this.rebuyDate = rebuyDate;
+    }
+
+    public void addVehicle(Vehicule vehicule){
+        this.lVehicle.add(vehicule);
+    }
+
+    public void rmVehicle(Vehicule vehicule){
+        this.lVehicle.remove(vehicule);
     }
 }
